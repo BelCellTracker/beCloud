bdcd16e938195e9d82280768ae572198e101f2d2
BelnetmonTracker <41490079+BelnetmonTracker@users.noreply.github.com>
2019-07-16 21:07:33 +0300

    Updated: 2019-07-16 21:07:33 +03:00

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ -f .env ]; then
+	export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+cd "$(dirname "$0")"
+LOCKFILE=".lock"
+
+if [ -f "$LOCKFILE" ] && kill -0 "$(cat $LOCKFILE)" 2>/dev/null; then
+	echo "Updating"
+	exit 1
+fi
+
+echo $$ > $LOCKFILE
+
+git pull
+
+./$UPDATER
+
+git add -A
+git commit --no-gpg-sign -a -m "Updated: $(date +"%Y-%m-%d %T %:z")"
+git push
+
+rm $LOCKFILE
